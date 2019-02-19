ecb324b9e5c6ea95df5680c3c29657992705e9ff
Marko Koskinen <markokoskinen2037@gmail.com>
2019-02-19 12:59:26 +0200

    create MockController for scoreboards

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import { LoginController } from './controllers'
+import { LoginController, MockController } from './controllers'
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/login', LoginController)
+app.use('/mock', MockController)
 
 app.get('/', (req, res) => {
   res.json({
