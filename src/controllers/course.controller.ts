c509e1e6226b34e92cd36ec040166b86b22c3027
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-03-18 15:49:09 +0200

    trying to join teachingInstance

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -3,7 +3,8 @@ import { createCourse } from '../services/courseService'
 
 const router: Router = Router()
 
-router.post('/', (req: Request, res: Response) => {
+// ./courses/
+router.post('/kuuba', (req: Request, res: Response) => {
   const { body } = req
   console.log(req.body)
   createCourse(req.cookies._kisallioppiminen_server_session, body)
